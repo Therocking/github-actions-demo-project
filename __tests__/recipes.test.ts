9aeeb6825f3d29ee7846a8da3d3a1cff88ba0474
Jose A. Genao <127759446+Therocking@users.noreply.github.com>
2025-07-20 13:13:35 -0400

    Update recipes.test.ts

--- __tests__/recipes.test.ts
+++ __tests__/recipes.test.ts
@@ -24,7 +24,7 @@ describe("Recipes API Endpoints", () => {
     
     // The data object must have a title
 
-    expect(res.body.data.title).toBeFalsy();
+    expect(res.body.data.title).toBeTruthy();
 
     // The data object must have an array on ingredients that is a string
 
